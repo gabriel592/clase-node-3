caab94e16628f94ce50d7c461c2f190651c4d3b8
gabriel592 <gabriel592>
2020-10-24 14:12:08 -0600

    pagina web

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-   res.send('Hola Mundo!');
-  //res.sendFile(path.join(__dirname + '/html/index.html'));;
+//res.send('Hola Mundo!');
+  res.sendFile(path.join(__dirname + '/html/index.html'));;
 });
 
 app.listen(port, () => {
